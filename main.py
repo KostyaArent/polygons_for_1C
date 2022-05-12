9c2c57e736a6ebfb8b611318be199290806c799c
katrepalin <trepalin.konstantin@gmail.com>
2022-05-12 13:34:49 +0300

    code cleaning2

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, Request, HTTPException
+from a2wsgi import ASGIMiddleware
+
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+
+
 from calculation import get_occurrence, calc_main_alter
 import json
 
@@ -10,12 +14,11 @@ app = FastAPI()
 
 @app.post("/multipoligon/intersection")
 async def getInformation(info : Request):
-    #try:
-    req_info = await info.json()
-    # req_info = dict(req_info)
-    result = calc_main_alter(req_info['fields'], req_info['districts'])
-    headers = {"X-Cat-Dog": "alone", "Content-Language": "en-en"}
-    return JSONResponse(content=result, headers=headers)
-    # except:
-    #     raise HTTPException(status_code=422, detail="Unprocessable Entity")
-    # 
+    try:
+        req_info = await info.json()
+        result = calc_main_alter(req_info['fields'], req_info['districts'])
+        headers = {"X-Cat-Dog": "alone", "Content-Language": "en-en"}
+        return JSONResponse(content=result, headers=headers)
+    except:
+        raise HTTPException(status_code=422, detail="Unprocessable Entity")
+    
